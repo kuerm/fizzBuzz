160078e28cc59ad908adbe514d19a30d61655dcd
Kuerm <kuerm@gmx.net>
2024-02-07 11:37:31 +0100

    Throw exception when input is below 0

--- src/test/java/ch/css/m3000/fizzBuzz/FizzBuzzTest.java
+++ src/test/java/ch/css/m3000/fizzBuzz/FizzBuzzTest.java
@@ -3,12 +3,13 @@ package ch.css.m3000.fizzBuzz;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 public class FizzBuzzTest {
     @Test
     void fizzBuzzWhen2() {
 
-        String actual = dada(2);
+        String actual = fizzBuzz(2);
 
         String expected = "1, 2";
         assertThat(actual).isEqualTo(expected);
@@ -18,13 +19,22 @@ public class FizzBuzzTest {
     void fizzBuzzWhenInput1() {
         int input = 1;
 
-        String actual = dada(input);
+        String actual = fizzBuzz(input);
 
         String expected = "1";
         assertThat(actual).isEqualTo(expected);
     }
 
-    private String dada(int input) {
+    @Test
+    void fizzBuzzWhenInputIsBelow1() {
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> fizzBuzz(0));
+    }
+
+    private String fizzBuzz(int input) {
+        if (input < 1) {
+            throw new IllegalArgumentException("Only numbers > 1 are allowed. Your Input was %d".formatted(input));
+        }
         StringBuilder result = new StringBuilder();
 
         for (int i = 1; i <= input; i++) {
