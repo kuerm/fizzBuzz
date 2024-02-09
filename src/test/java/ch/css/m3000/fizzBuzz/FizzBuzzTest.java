c260ca951e3232dbe608eaf8b4970d073e8e560e
Kuerm <kuerm@gmx.net>
2024-02-09 14:03:02 +0100

    Unrelated comment

--- src/test/java/ch/css/m3000/fizzBuzz/FizzBuzzTest.java
+++ src/test/java/ch/css/m3000/fizzBuzz/FizzBuzzTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
-public class FizzBuzzTest {
+class FizzBuzzTest {
     @Test
     void fizzBuzzWhen2() {
 
